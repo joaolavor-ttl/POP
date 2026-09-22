9d533cb2e3af9d8b4d34814bada9fabc6381cecd
joaolavor-ttl <miguel.lavor@academico.ifpb.edu.br>
2026-09-22 11:19:09 -0300

    Refactor cppPM1.cc to improve readability and maintainability

--- CPPPM/cppPM1.cc
+++ CPPPM/cppPM1.cc
@@ -1,9 +1,28 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(){
+int main() {
+    // Otimização de I/O
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
+    string s;
+    if (getline(cin, s)) {
+        string decodificado = "";
+        
+        for (size_t i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                decodificado += ' ';
+            } else {
+                decodificado += s[i + 1];
+                i++; 
+            }
+        }
+        
+        cout << decodificado << "\n";
+    }
+
     return 0;
 }
