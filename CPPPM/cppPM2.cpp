398466db940bc3b3590783aa9ee68da0e6ec29c2
joaolavor-ttl <miguel.lavor@academico.ifpb.edu.br>
2026-09-23 23:28:16 -0300

    Fix typo in length function and improve fada function logic for word detection

--- CPPPM/cppPM2.cpp
+++ CPPPM/cppPM2.cpp
@@ -2,22 +2,48 @@
 using namespace std;
 
 bool eh_letra(char c){
-    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    return (c >= 'a' and c <= 'z') || (c >= 'A' and c <= 'Z');
 }
 bool fada(const string& s){
-    int N =s.lenght();
+    int N =s.length();
+    for(int i=0;i+3<N;i++){
+        if(tolower(s[i])   == 'f' and
+            tolower(s[i+1]) == 'a' and
+            tolower(s[i+2]) == 'd' and
+            tolower(s[i+3]) == 'a'){
+                bool ok_left=(i==0) or !eh_letra(s[i-1]);
+                bool ok_right=(i+4==N) or !eh_letra(s[i+4]);
 
-    
+                if(ok_left and ok_right){
+                    return true;
+                }
+            }
+    }
+    return false;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int N;
+    if(!(cin>>N)) return 0;
+    
+    string s;
+    getline(cin, s);
+
+    int ans=0;
+    while(N-- and getline(cin,s)){
+        if(fada(s)){
+            ans++;
+        }
+    }
+    cout<<ans<<'\n';
+
     return 0;
 }
 
-
+//way 2
 /*
 int main(){
     ios::sync_with_stdio(false);
@@ -48,6 +74,8 @@ int main(){
     return 0;
 }
 */
+
+//way 3
 /*
 int main(){
     ios::sync_with_stdio(false);
