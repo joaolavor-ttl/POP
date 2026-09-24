4099ca06f347aca37a2526f156805608900306bc
joaolavor-ttl <miguel.lavor@academico.ifpb.edu.br>
2026-09-23 23:49:13 -0300

    Refactor eh_fada function for clarity and improve main logic for input handling

--- CPPPM/cppPM2.cpp
+++ CPPPM/cppPM2.cpp
@@ -1,49 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool eh_letra(unsigned char c) {
-    if (c > 127) return true;
     return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
-bool fada(const string& s) {
-    int N = s.length();
-    for (int i = 0; i + 3 < N; i++) {
-        if (tolower((unsigned char)s[i])   == 'f' &&
-            tolower((unsigned char)s[i+1]) == 'a' &&
-            tolower((unsigned char)s[i+2]) == 'd' &&
-            tolower((unsigned char)s[i+3]) == 'a') {
 
-            bool ok_left  = (i == 0)     || !eh_letra((unsigned char)s[i-1]);
-            bool ok_right = (i + 4 == N) || !eh_letra((unsigned char)s[i+4]);
+bool eh_fada(const string& s) {
+    for (int i = 0; i + 3 < (int)s.size(); i++) {
+        char a = tolower((unsigned char)s[i]);
+        char b = tolower((unsigned char)s[i + 1]);
+        char c = tolower((unsigned char)s[i + 2]);
+        char d = tolower((unsigned char)s[i + 3]);
 
-            if (ok_left && ok_right) {
+        if (a == 'f' && b == 'a' && c == 'd' && d == 'a') {
+            bool esquerda = (i == 0 || !eh_letra((unsigned char)s[i - 1]));
+            bool direita = (i + 4 == (int)s.size() || !eh_letra((unsigned char)s[i + 4]));
+
+            if (esquerda && direita) {
                 return true;
             }
         }
     }
+
     return false;
 }
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int N;
-    while (cin >> N) {
+    cin >> N;
+    cin.ignore();
+
+    int resposta = 0;
+
+    for (int i = 0; i < N; i++) {
         string s;
         getline(cin, s);
 
-        int ans = 0;
-        while (N--) {
-            if (!getline(cin, s)) break;
-            if (!s.empty() && s.back() == '\r') {
-                s.pop_back();
-            }
-            if (fada(s)) {
-                ans++;
-            }
+        if (eh_fada(s)) {
+            resposta++;
         }
-        cout << ans << '\n';
     }
+
+    cout << resposta << '\n';
+
     return 0;
 }
 
