c1f119590fc105b427fef80c36ea87e5bf132801
joaolavor-ttl <miguel.lavor@academico.ifpb.edu.br>
2026-09-23 23:12:02 -0300

    Refactor cppPM2.cpp to use regex for case-insensitive word search

--- CPPPM/cppPM2.cpp
+++ CPPPM/cppPM2.cpp
@@ -1,11 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool eh_letra(char c){
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+bool fada(const string& s){
+    int N =s.lenght();
+
+    
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-
     return 0;
 }
+
+
+/*
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, ans =0;
+    cin>>N;
+
+    string s;
+    getline(cin, s);//clean the '\n'
+
+    while(N--){
+        getline(cin, s);
+        for(char &c:s){
+            c=tolower(c);
+            if(!isalpha(c)) c=' ';
+        }
+        stringstream ss(s);
+        string palavra;
+        bool find=false;
+
+        while(ss>>palavra){
+            if(palavra=="fada") find =true;
+        }
+        if(find) ans++;
+    }
+    cout<<ans<<'\n';
+    return 0;
+}
+*/
+/*
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, ans=0;
+    string s;
+    regex r("\\bfada\\b", regex::icase);
+    for(cin>>n>>ws; n-- && getline(cin, s); ){
+        ans+=regex_search(s, r);
+
+    cout<<ans<<'\n';
+    }
+    return 0;
+}
+*/
